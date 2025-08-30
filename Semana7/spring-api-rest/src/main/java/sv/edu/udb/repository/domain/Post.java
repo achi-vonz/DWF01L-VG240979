8199a2eb29f81cb00511f0b3d25fe0fa50638501
DavidAlvar3z <179424988+achi-vonz@users.noreply.github.com>
2025-08-30 14:24:40 -0600

    Subiendo semana7

---
+++ Semana7/spring-api-rest/src/main/java/sv/edu/udb/repository/domain/Post.java
@@ -0,0 +1,24 @@
+package sv.edu.udb.repository.domain;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+@Getter
+@Setter
+@Entity //Anotacion para marcar que es una entidad de negocio
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Post {
+    @Id //definicion del id (es una anotacion obligatoria si se usa entity)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) //Generacion automatica de id
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private LocalDate postDate;
+}
